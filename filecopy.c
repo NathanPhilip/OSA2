9d21a6bef7ea5083487694ffb30a9173c253185a
EthoKurza <36766454+EthoKurza@users.noreply.github.com>
2018-10-07 22:40:38 -0400

    filecopy.c
    working filecopy version

---
+++ filecopy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	//declare variables
+	FILE *source, *copy;
+	//char fileCopy = *argv[2],file = argv[1],tempWord="";
+	char word=' ';
+	 
+	
+	source = fopen(argv[1], "r");
+	
+	//check if file can be opened 
+	if(source == NULL){
+		printf("Error file cannot be opened...\n");
+		printf("Enter any key to exit...\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	copy = fopen(argv[2], "w");
+	//check if file can be created
+	if(copy == NULL){
+		printf("Error copied file cannot be created...\n");
+		printf("Enter any key to exit program....\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	while((word = fgetc(source)) != EOF){
+		fputc(word, copy);
+	}
+	
+	printf("File copied.\n");
+	
+	fclose(source);
+	fclose(copy);
+}//end of main
